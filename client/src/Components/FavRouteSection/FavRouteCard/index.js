49711b1b9faac1d07c49889f2a3e2d4cb63260c5
mauraparker02 <mauraparker02@gmail.com>
2020-07-16 14:02:12 -0400

    styling categories

--- client/src/Components/FavRouteSection/FavRouteCard/index.js
+++ client/src/Components/FavRouteSection/FavRouteCard/index.js
@@ -33,7 +33,8 @@ class FavRouteCard extends Component {
                   </div>
                   <div className="card-content">
                   <div className="card-action">
-                    <span className="card-title"> {this.props.route.name} <b>{this.props.route.price_category}</b> <b>{this.props.route.activities}</b></span>
+                    <span className="card-title"> {this.props.route.name}</span>
+                    <b>{this.props.route.price_category}</b> <b>{this.props.route.activities}</b>
                     <p>{this.props.route.description}</p>
                   </div>
                   <div className="card-action">
